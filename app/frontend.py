93ebd0889359f767ef1c21814647ea8055bc4bf6
Julio Lacerda <julio.castro.lacerda@gmail.com>
2018-01-18 14:29:44 +0000

    Add Unbabel API (unbabel-py). Create some extra functionalities on top of it (getting translations using grequests for instance)

--- app/frontend.py
+++ app/frontend.py
@@ -1,19 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from hackernews_api import HackerNewsAPI
+from unbabel_api import UnbabelAPI
 
+hn_api = HackerNewsAPI()
+ub_api = UnbabelAPI()
 
 # create frontend blueprint
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route("/")
 def home():
-	hn_api = HackerNewsAPI()
 	topstories = hn_api.get_detailed_topstories()
 	return render_template('index.html', context={'topstories': topstories})
 
 
 @frontend.route("/detail/<int:story_id>")
 def story(story_id):
-	hn_api = HackerNewsAPI()
 	story = hn_api.get_story_with_comments(story_id)
 	return render_template('detail.html', context=story)
+
+
+@frontend.route("/request-story-translation/<int:story_id>")
+def request_translation(story_id):
+	story = hn_api.get_story_with_comments(story_id)
+	texts = [story['story']['title'], [comment['text'] for comment in story['comments']]]
+	request_translation_response = ub_api.bulk_translate(texts)
+	return jsonify([t.uid for t in request_translation_response])
+
+
+@frontend.route("/get-translation/<request_list>")
+def get_translations(request_list):
+	request_list = map(str, request_list.rstrip(',').split(','))
+	translation_response = ub_api.get_translations(request_list)
+	return jsonify(translation_response)
+
---
+++ app/unbabel_api.py
@@ -0,0 +1,49 @@
+import requests
+import grequests
+from unbabel.api import UnbabelApi
+
+
+def get_translations_exception_handler(request, exception):
+	print 'Response failed'
+
+
+class UnbabelAPI(object):
+	USERNAME = 'fullstack-challenge'
+	API_KEY = "9db71b322d43a6ac0f681784ebdcc6409bb83359"
+	GET_TRANSLATION_ENDPOINT = 'translation/{0}'
+
+	def __init__(self, *args, **kwargs):
+		self.api = UnbabelApi(username=self.USERNAME, api_key=self.API_KEY, sandbox=True)
+
+	def build_url(self, endpoint=''):
+		return '%s%s' % (self.api.api_url, endpoint)
+
+	def bulk_translate(self, texts, source_lang="en", target_lang="pt"):
+		translations = []
+		for t in texts:
+			translations.append({
+			    "text": t,
+			    "source_language": source_lang,
+			    "target_language": target_lang
+			})
+
+		return self.api.post_bulk_translations(translations)
+
+	def translate(self, text, source_lang="en", target_lang="pt"):
+		return self.api.post_translations(text, source_lang, target_lang)
+
+	def get_translations(self, request_list):
+
+		# comment urls
+		translation_urls = [self.build_url(self.GET_TRANSLATION_ENDPOINT.format(pk)) for pk in request_list]
+		print translation_urls
+		# prepare the async translation requests
+		set_of_translation_requests = (grequests.get(u, headers=self.api.headers) for u in translation_urls)
+
+		# fetch them all!
+		full_translations = grequests.map(set_of_translation_requests, exception_handler=get_translations_exception_handler)
+		
+		full_translations_data = []
+		if all(map(lambda x: True if x.status_code == 200 else False, full_translations)):
+			full_translations_data = [t.json() for t in full_translations if t is not None]
+		return full_translations_data
